e64f42e905926606999c5e1d4f0bbda188ea4658
DuRey <ldureyl@mail.ru>
2024-01-12 20:34:17 +0300

    Fixed default handler in main branch

--- handlers/general.py
+++ handlers/general.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Router, F
 from aiogram.types import Message
 from aiogram.fsm.state import default_state
 from aiogram.fsm.context import FSMContext
-from aiogram.filters import StateFilter, Command
+from aiogram.filters import StateFilter, Command, invert_f
 from keyboards.reply import register_keyboard
 from keyboards.inline import edit_user_info_keyboard
 from stategroups.stategroups import UserRegistration, EditUserInfo
@@ -11,31 +11,31 @@ from db import get_nutrients
 router = Router()
 
 
-@router.message(Command("start"))
+@router.message(StateFilter(default_state), Command("start"))
 async def start_command(message: Message, bot: Bot) -> None:
     await bot.send_message(message.from_user.id, f"Нажмите на кнопку \"Ввести данные\".\nНужно будет ввести возраст, вес, рост и выбрать пол.", reply_markup=register_keyboard)
     await bot.delete_message(chat_id=message.from_user.id, message_id=message.message_id)
 
 
-@router.message(F.text == "Ввести данные")
+@router.message(StateFilter(default_state), F.text == "Ввести данные")
 async def set_age(message: Message, state: FSMContext, bot: Bot) -> None:
     await bot.send_message(message.from_user.id, f"Введите ваш возраст:") 
     await state.set_state(UserRegistration.age)
 
 
-@router.message(F.text == "Изменить данные")
+@router.message(StateFilter(default_state), F.text == "Изменить данные")
 async def edit_user_info(message: Message, state: FSMContext, bot: Bot):
     await state.set_state(EditUserInfo.choose)
     await bot.send_message(message.from_user.id, "Что вы хотите изменить?", reply_markup=edit_user_info_keyboard)
 
 
-@router.message(F.text == "Проверить норму")
+@router.message(StateFilter(default_state), F.text == "Проверить норму")
 async def get_daily_PFH_norm(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, "Осталось:\nБелки: {0}\nЖиры: {1}\nУглеводы: {2}".format(*get_nutrients(message.from_user.id)))
 
 
 
-@router.message(~StateFilter(default_state), ~F.text[0] == '/')
+@router.message(~StateFilter(default_state), invert_f(F.text[0] == '/'))
 async def wrong_data(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, "Данные введены неправильно")
 
